2854bf980cf70e6bdbfc6fb4ab90b1070372a228
Naman Vyas <116788674+namangitanjali@users.noreply.github.com>
2023-09-20 14:37:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+app.enable('trust proxy');
 
 const errorMiddleware = require("./middleware/error");
 
